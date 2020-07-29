58bf0e6a2e26b539423aea5e679930c498a3b045
Greyson Elkins <greysonelkins@gmail.com>
2020-07-29 13:16:58 -0600

    Create 10 faux users

---
+++ test/faux-data.js
@@ -0,0 +1,34 @@
+"users:" [
+  {
+    "id": 1, 
+    "name": 'Greyson Elkins'
+  },  {
+    "id": 2,
+    "name": 'Josh Sevy'
+  },  {
+    "id": 3,
+    "name": 'Nick Hart'
+  },  {
+    "id": 4,
+    "name": 'Aaron DB'
+  }, {
+    "id": 5,
+    "name": 'Leigh Larson'
+  }, {
+    "id": 6, 
+    "name": 'Bob Gu'
+  },  {
+    "id": 7, 
+    "name": 'Will Mitchel'
+  }, {
+    "id": 8,
+    "name": 'Your mom'
+  }, {
+    "id": 9,
+    "name": 'Mike Viola'
+  },
+  {
+    "id": 10,
+    "name": 'Marschall McCluhan'
+  }
+]
