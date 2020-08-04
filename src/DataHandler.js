fdcae7ad0defe035de19666c29933a035dba9940
Greyson Elkins <greysonelkins@gmail.com>
2020-08-04 00:50:22 -0600

    Provide more thorough testing for GET and POST, begin DELETE tests

--- src/DataHandler.js
+++ src/DataHandler.js
@@ -8,12 +8,6 @@ class DataHandler {
       .catch(err => console.log(err))
   }
 
-  getAllData (hotel) {
-    let args = Array.from(arguments);
-    args.shift();
-    return args.forEach(argument => this.getData(argument, hotel))
-  }
-
   makeBooking(booking) {
     fetch(
       `https://fe-apps.herokuapp.com/api/v1/overlook/1904/bookings/bookings`, 
@@ -24,36 +18,33 @@ class DataHandler {
         },
         body: JSON.stringify(booking)
       })
-      .then(response => response.json)
-      .then(() => alert(`Thanks for planning a trip with us, we look` +
-      ' forward to your stay!'))
+      .then(response => {
+        if (response.ok) {
+          alert(`Thanks for planning a trip with us, we look` +
+            ' forward to your stay!')
+        } else {
+          throw new Error('Something went wrong')
+        }
+      }) 
       .catch(error => {
         alert(`Something went wrong, please try again`)
         console.log(error)
       })
   }
 
-  // deleteBooking(event) {
-  //   fetch(
-  //     `https://fe-apps.herokuapp.com/api/v1/overlook/1904/bookings/bookings/${event.target.id}`,
-  //     {
-  //       method: 'DELETE'
-  //     })
-  //     .then(response => response.json)
-  // }
-
-  deleteBooking(event) {
+  deleteBooking(id) {
     fetch(`https://fe-apps.herokuapp.com/api/v1/overlook/1904/bookings/bookings`, {
       method: "DELETE",
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({"id": event}),
+      body: JSON.stringify({"id": id}),
     })
-      .then((res) => res.json())
-      .then((data) => {
-        console.log('Success', data);
-      })
-      .catch((err) => console.log(err));
+      // .then(response => response.json())
+      // .then(data => {
+      //   console.log(data);
+      // })
+      // .catch((err) => console.log(err));
   }
+  // also, will this response be empty if successful?
 
 }
 
--- test/DataHandler-test.js
+++ test/DataHandler-test.js
@@ -6,6 +6,7 @@ chai.use(spies)
 
 import Hotel from "../src/Hotel"
 import DataHandler from '../src/DataHandler'
+import { bookings } from './faux-data'
 
 describe("Data Handler", () => {
 
@@ -15,8 +16,10 @@ describe("Data Handler", () => {
   let fauxDataHandler
 
   beforeEach(() => {
+    dataHandler = new DataHandler()
     fauxDataHandler = new DataHandler
     mockResponse = {
+      ok: 'value',
       then: (method) => {
         method(mockResponse)
         return mockResponse
@@ -27,11 +30,11 @@ describe("Data Handler", () => {
     }
     Object.prototype.json;
     overlook = new Hotel()
-    dataHandler = new DataHandler()
     chai.spy.on(global, 'fetch', () => {
       return mockResponse
     })
     chai.spy.on(overlook, 'storeData', () => {})
+    chai.spy.on(global, "alert", () => {})
     chai.spy.on(Object.prototype, 'json', () => {})
     chai.spy.on(fauxDataHandler, 'getData', ()=> {})
   })
@@ -49,15 +52,73 @@ describe("Data Handler", () => {
       )
     })
 
+    it('should json the response', () => {
+      dataHandler.getData('users', overlook) 
+      expect(Object.prototype.json).to.have.been.called(1)
+    })
+
     it('should store data once it fetches', () => {
       dataHandler.getData('rooms', overlook) 
       expect(overlook.storeData).to.have.been.called(1)
       expect(global.json).to.have.been.called(1)
     })
+  })
 
-    it('should be able to iterate over getData for multiple apis', () => {
-      fauxDataHandler.getAllData(overlook, 'users', 'rooms')
-      expect(fauxDataHandler.getData).to.have.been.called(2)
+  describe('POST', () => {
+    it('should post to the bookings API', () => {
+      dataHandler.makeBooking()
+      expect(fetch).to.have.been.called(1)
+      expect(fetch).to.have.been.called.with(
+        'https://fe-apps.herokuapp.com/api/v1/overlook/1904/bookings/bookings'
+      )
+    })
+
+    it('should include a header with method, headers, and body', () => {
+      dataHandler.makeBooking(bookings[0])
+      expect(fetch).to.have.been.called.with({
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(bookings[0])
+      })
+    })
+
+    it('should alert the user if the post was successfully made', () => {
+      dataHandler.makeBooking(bookings[0])
+      expect(alert).to.have.been.called(1)
+      expect(alert).to.have.been.called.with(
+        'Thanks for planning a trip with us, we look forward to your stay!'
+      )
+    })
+
+    it('should be an error if the response is not ok', () => {
+      mockResponse.ok = undefined;
+      expect(dataHandler.makeBooking).to.throw('Something went wrong')
+    })
+  })
+
+  describe('DELETE', () => {
+    
+    it('should make a fetch request', () => {
+      dataHandler.deleteBooking()
+      expect(fetch).to.have.been.called(1)
+    })
+
+    it('should fetch to the bookings API', () => {
+      dataHandler.deleteBooking()
+      expect(fetch).to.have.been.called.with(
+        `https://fe-apps.herokuapp.com/api/v1/overlook/1904/bookings/bookings`
+      )
+    })
+
+    it('should be called with method, headers, and body', () => {
+      dataHandler.deleteBooking(bookings[0].id)
+      expect(fetch).to.be.called.with({
+        method: "DELETE",
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ "id": bookings[0].id }),
+      })
     })
   })
 })
